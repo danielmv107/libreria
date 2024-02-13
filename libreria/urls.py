ce7d8a9d1c0c3e6f3a05cc8db963ac52d48268a7
DanielMejia <daniellmejia107@gmail.com>
2024-02-13 23:37:55 +0100

    se agregaron nuevas path al archivo urls de liberia

--- libreria/urls.py
+++ libreria/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear'),
+    path('libros/editar', views.editar, name='editar'),
 ]
