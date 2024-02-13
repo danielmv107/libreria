e85915d96b1b5011274b607d353d8f61c5403f9f
DanielMejia <daniellmejia107@gmail.com>
2024-02-13 23:36:10 +0100

    se agregaron nuevas funciones como crear y editar

--- libreria/views.py
+++ libreria/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
+
+def libros(request):
+    return render(request, 'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')
