79e6bb0780fe34b2582d0bd9adc2f44608f7e5bf
DanielMejia <daniellmejia107@gmail.com>
2024-02-14 10:53:06 +0100

    se creo el modelo libro, antes de hacer las migraciones al DB

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from typing import Any
 
 # Create your models here.
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name="imagen", null=True)
+    descripcion = models.TextField(verbose_name="Descripcion", null=True)
