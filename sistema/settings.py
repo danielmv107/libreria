d3191d18ff85eec8a67be1e230ce2304865f5781
DanielMejia <daniellmejia107@gmail.com>
2024-02-14 10:19:57 +0100

    conexion con la base de datos de nombre libreria2

--- sistema/settings.py
+++ sistema/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backkends.mysql',
+        'NAME': 'libreria2',
+        'USER': 'root', 
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
